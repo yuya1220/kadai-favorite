db35f20224d6a1880364fcf78a2fd42dc016e566
yuya.sugiuchi <yuya.zerostart@gmail.com>
2020-03-14 03:40:12 +0000

    post

--- app/controllers/toppage_controller.rb
+++ app/controllers/toppage_controller.rb
@@ -1,4 +1,8 @@
 class ToppageController < ApplicationController
   def index
+    if logged_in?
+      @micropost = current_user.microposts.build #form_with用
+      @microposts = current_user.microposts.order(id: :desc).page(params[:page])
+    end
   end
 end
